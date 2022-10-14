159aec952fd97abab7e77fb163bcae052ec994a8
Gartox <ewan71230@gmail.com>
2022-10-14 16:30:52 +0200

    Still install script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 sudo pacman -Syu --needed xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3-gaps i3lock polybar picom rofi
 
-sudo pacman -Syu --needed alacritty fzf ripgrep bat bat-extras tmux neovim firefox
+sudo pacman -Syu --needed alacritty fzf ripgrep bat bat-extras tmux neovim firefox nitrogen
 
 # Install yay
 sudo pacman -S --needed git base-devel
@@ -19,5 +19,12 @@ makepkg -si
 popd
 
 yay -Syu nerd-fonts-jetbrains-mono 
+
+sudo localectl set-x11-keymap fr
+sudo systemctl enable lightdm
+
+# Install packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 ./create_links.sh
