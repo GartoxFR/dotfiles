09df3e9dff7ba880b208227e6ceb6f1701d99e73
Gartox <ewan71230@gmail.com>
2022-10-14 15:57:42 +0200

    Some changes

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-sudo pacman -Syu --needed xorg lightdm lightdm-slick-greeter i3-gaps i3lock polybar picom rofi
+sudo pacman -Syu --needed xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3-gaps i3lock polybar picom rofi
 
 sudo pacman -Syu --needed fzf ripgrep bat bat-extras tmux neovim firefox
 
@@ -19,3 +19,5 @@ makepkg -si
 popd
 
 yay -Syu nerd-fonts-jetbrains-mono 
+
+./create_links.sh
