06fee110b12cdd52bcbd55d11a30488042cf9cec
Gartox <ewan71230@gmail.com>
2022-10-14 14:56:27 +0200

    Change install

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
-sudo pacman -Syu git 
+sudo pacman -Syu --needed git zsh
 
 #Ohmyzsh installation
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-sudo pacman -Syu xorg lightdm lightdm-slick-greeter i3-gaps i3lock polybar picom rofi
+sudo pacman -Syu --needed xorg lightdm lightdm-slick-greeter i3-gaps i3lock polybar picom rofi
 
-sudo pacman -Syu fzf ripgrep bat bat-extras tmux neovim firefox
+sudo pacman -Syu --needed fzf ripgrep bat bat-extras tmux neovim firefox
 
 # Install yay
 pushd /tmp
